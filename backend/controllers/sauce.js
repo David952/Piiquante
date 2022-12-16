69caf1a948cd6b9cbf08b4276a74640aa97855a8
David952 <david.amorim@live.fr>
2022-12-16 04:16:31 +0100

    Modifications fichiers (controllers)

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,42 +1,41 @@
 const sauceModels = require('../models/sauce');
 const fs = require('fs');
-const { json } = require('body-parser');
 
 /**
  * Middleware de création d'une sauce
  * @param req - On récupère une sauce, l'id de l'utilisateur, le protocole 'HTTP', l'hôte du serveur et le nom de l'image
- * @param res - On renvoi un message si l'enregistrement a été validé ou non
+ * @param res - Si l'enregistrement a été validé le status sera "Created" sinon en "Bad Request"
  */
 exports.createSauce = (req, res) => {
     const sauceObject = JSON.parse(req.body.sauce);
     delete sauceObject._id;
-    delete sauceObject._userId;
     const sauce = new sauceModels({
         ...sauceObject,
-        _userId: req.auth.userId,
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         likes: 0,
         dislikes: 0,
+        usersLiked: [],
+        usersDisliked: []
     });
     sauce.save()
-    .then(() => { res.status(201).json({ message: 'Sauce enregistré !'})})
+    .then(() => { res.status(201).json({ message: 'Sauce enregistré !' })})
     .catch(error => { res.status(400).json({ error })})
 }
 
 /**
  * Middleware de récupération de toutes les sauces
- * @param res - On renvoi un message si on a bien toutes les sauces ou non
+ * @param res - Si on récupère toutes les sauces le status sera "OK" sinon "Not Found"
  */
 exports.getAllSauces = (req, res) => {
     sauceModels.find()
         .then(sauces => res.status(200).json(sauces))
-        .catch(error => res.status(400).json({ error }));
+        .catch(error => res.status(404).json({ error }));
 }
 
 /**
  * Middleware de récupération d'une seule sauce
  * @param req - On récupère l'id d'une sauce
- * @param res - On renvoi un message si on a une sauce ou non
+ * @param res 
  */
 exports.getOneSauce = (req, res) => {
     sauceModels.findOne({
@@ -49,12 +48,12 @@ exports.getOneSauce = (req, res) => {
 /**
  * Middleware de modification d'une sauce
  * @param req - On récupère l'id puis l'ensemble des éléments du 'body'
- * @param res - On renvoi un message si on a modifié une sauce ou non
+ * @param res 
  */
 exports.modifySauce = (req, res) => {
     sauceModels.findOne({ _id: req.params.id })
         .then(sauce => {
-            if(sauce.userId !== req.auth.userId) return res.status(401).json({ message: 'Opération non-autorisée'});
+            if(sauce.userId !== req.auth.userId) return res.status(401).json({ message: 'Opération non-autorisée' });
 
             const sauceObject = req.file ? {
                 ...JSON.parse(req.body.sauce), 
@@ -63,48 +62,51 @@ exports.modifySauce = (req, res) => {
 
             delete sauceObject.userId;
 
-            sauceModels.updateOne({
-                _id: req.params.id
-            }, {
-                // L'opérateur spread "..." permet de faire une copie de tous les éléments de 'req.body'
-                ...sauceObject,
-                _id: req.params.id
+            const filename = sauce.imageUrl.split('/images/')[1];
+            fs.unlink(`images/${filename}`, () => {
+                sauceModels.updateOne({
+                    _id: req.params.id
+                }, {
+                    // L'opérateur spread "..." permet de faire une copie de tous les éléments de 'sauceObject'
+                    ...sauceObject,
+                    _id: req.params.id
+                })
+                .then(() => res.status(200).json({ message: 'Sauce modifié !' }))
+                .catch(error => res.status(400).json({ error }));
             })
-            .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
-            .catch(error => res.status(400).json({ error }));
         });
 }
 
 /**
  * Middleware de suppression d'une sauce
  * @param req - On récupère l'id et on fait une vérification entre l'id d'un utilisateur et celle de la sauce
- * @param res - On renvoi un message si une sauce a été supprimé ou non
+ * @param res
  */
 exports.deleteSauce = (req, res) => {
     sauceModels.findOne({ _id: req.params.id })
     .then(sauce => {
         if (sauce.userId != req.auth.userId) {
-            res.status(401).json({ message : 'Non-autorisé'});
-        } else {
-            const filename = sauce.imageUrl.split('/images')[1];
-            fs.unlink(`images/${filename}`, () => {
-                sauceModels.deleteOne({ _id: req.params.id })
-                            .then(() => res.status(200).json({ message: 'Sauce supprimé !'}))
-                            .catch(error => res.status(401).json({ error }));
+            res.status(401).json({ message : 'Non-autorisé' });
+        } 
+        
+        const filename = sauce.imageUrl.split('/images')[1];
+        fs.unlink(`images/${filename}`, () => {
+            sauceModels.deleteOne({ _id: req.params.id })
+                .then(() => res.status(200).json({ message: 'Sauce supprimé !' }))
+                .catch(error => res.status(401).json({ error }));
             });
-        }
     })
-
+    
     .catch(error => { res.status(500).json({ error })})
 }
 
 /**
  * Middleware j'aime ou je n'aime pas une sauce
  * @param req - On récupère l'id de l'utilisateur, de la sauce et de 'like'
- * @param res - On renvoi un message si on aime une sauce, on aime pas une sauce ou si on retire un like ou un dislike
+ * @param res
  */
 exports.likesAndDislikes = (req, res) => {
-    sauceModels.findOne({ _id: req.params.id})
+    sauceModels.findOne({ _id: req.params.id })
         .then(sauce => {
             switch(req.body.like) {
                 // Vérification que l'utilisateur n'a pas déjà liké la sauce (n'existe pas dans le tableau des utilisateurs)
@@ -117,7 +119,7 @@ exports.likesAndDislikes = (req, res) => {
                                 $push: { usersLiked: req.body.userId }
                             }
                         )
-                            .then(() => res.status(201).json({ message: "J'aime cette sauce !"}))
+                            .then(() => res.status(201).json({ message: "J'aime cette sauce !" }))
                             .catch((error) => res.status(400).json({ error }));
                     }
                     break;
@@ -131,7 +133,7 @@ exports.likesAndDislikes = (req, res) => {
                                 $push: { usersDisliked: req.body.userId }
                             }
                         )
-                            .then(() => res.status(201).json({ message: "Je n'aime pas cette sauce !"}))
+                            .then(() => res.status(201).json({ message: "Je n'aime pas cette sauce !" }))
                             .catch((error) => res.status(400).json({ error }));
                     }
                     break;
@@ -145,7 +147,7 @@ exports.likesAndDislikes = (req, res) => {
                                 $pull: { usersLiked: req.body.userId }
                             }
                         )
-                            .then(() => res.status(201).json({ message: "Votre 'j'aime' a bien été retiré"}))
+                            .then(() => res.status(201).json({ message: "Votre 'j'aime' a bien été retiré" }))
                             .catch((error) => res.status(400).json({ error }));
                     }
 
@@ -157,7 +159,7 @@ exports.likesAndDislikes = (req, res) => {
                                 $pull: { usersDisliked: req.body.userId }
                             }
                         )
-                            .then(() => res.status(201).json({ message: "Votre 'je n'aime pas' a bien été retiré"}))
+                            .then(() => res.status(201).json({ message: "Votre 'je n'aime pas' a bien été retiré" }))
                             .catch((error) => res.status(400).json({ error }));
                     }
                     break;
