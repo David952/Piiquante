0277225a74bc02459cdd6328a1b7b7281b8da3e3
David952 <david.amorim@live.fr>
2023-12-07 22:14:52 +0100

    Modification user (routes)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const userCtrl = require('../controllers/user');
 
+const limiter = require('../middleware/rateLimiter');
+
+router.use(limiter);
+
 // On cible le chemin puis on appelle le fichier user dans "controllers"
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login',  userCtrl.login);
 
 module.exports = router;
