88299a1591e329070f8ee36356df972bfb6278e1
David952 <david.amorim@live.fr>
2023-12-15 22:33:40 +0100

    Actualisation 'sauce' (models)

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -3,16 +3,16 @@ const mongoose = require('mongoose');
 // On crée un schéma de données qui contient les champs souhaités pour chaque Sauce
 const sauceSchema = mongoose.Schema({
   // ID de l'utilisateur qui a crée la sauce
-  userId: { type: String, required: true },
-  name: { type: String, required: true },
+  userId: { type: String },
+  name: { type: String },
   // Fabricant de la sauce
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
+  manufacturer: { type: String },
+  description: { type: String },
   // Le principal ingrédient épicé de la sauce
-  mainPepper: { type: String, required: true },
-  imageUrl: { type: String, required: true },
+  mainPepper: { type: String },
+  imageUrl: { type: String },
   // Note décrivant la sauce
-  heat: { type: Number, required: true },
+  heat: { type: Number },
   likes: { type: Number, default: 0 },
   dislikes: { type: Number, default: 0 },
   // Tableau des identifiants des utilisateurs qui ont aimé la sauce
@@ -20,14 +20,6 @@ const sauceSchema = mongoose.Schema({
   // Tableau des identifiants des utilisateurs qui n'ont pas aimé la sauce
   usersDisliked: { type: [String], default: [] },
 });
-
-sauceSchema.methods.joiValidate = function(obj) {
-	var Joi = require('joi');
-	var schema = {
-		heat: Joi.types.Number().min(1).max(10),
-	}
-	return Joi.validate(obj, schema);
-}
 
 // On exporte ce schéma en modèle utilisable
 module.exports = mongoose.model('Sauce', sauceSchema);
